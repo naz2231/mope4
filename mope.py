6cfe60e9e1a583c2a5f841c03456cdeb9decc669
naz2231 <33528344+naz2231@users.noreply.github.com>
2020-04-02 15:36:27 +0300

    Update mope.py

--- mope.py
+++ mope.py
@@ -67,61 +67,65 @@ def phisher_criterion(Y_matrix, d):
         return Fp < table_fisher(0.95, N, m, d)
     return Fp < table_fisher(0.95, N, m, d)
 
+while(True):
+    matrix_with_min_max_x = np.array([[10, 60], [-35, 15], [10, 15]])
+    m = 6
+    N = 8
+    norm_matrix = np.array(list(product("01", repeat=3)), dtype=np.int)
+    norm_matrix[norm_matrix == 0] = -1
+    norm_matrix = np.insert(norm_matrix, 0, 1, axis=1)
+    plan_matrix = np.empty((8, 3))
+    for i in range(len(norm_matrix)):
+        for j in range(1, len(norm_matrix[i])):
+            if j == 1:
+                if norm_matrix[i, j] == -1:
+                    plan_matrix[i, j-1] = 10
+                elif norm_matrix[i, j] == 1:
+                    plan_matrix[i, j-1] = 60
+            elif j == 2:
+                if norm_matrix[i, j] == -1:
+                    plan_matrix[i, j-1] = -35
+                elif norm_matrix[i, j] == 1:
+                    plan_matrix[i, j-1] = 15
+            elif j == 3:
+                if norm_matrix[i, j] == -1:
+                    plan_matrix[i, j-1] = 10
+                elif norm_matrix[i, j] == 1:
+                    plan_matrix[i, j-1] = 15
+    plan_matr = np.insert(plan_matrix, 0, 1, axis=1)
+    Y_matrix = np.random.randint(200 + np.mean(matrix_with_min_max_x, axis=0)[0],
+                                 200 + np.mean(matrix_with_min_max_x, axis=0)[1], size=(N, m))
+    mean_Y = np.mean(Y_matrix, axis=1)
+    combination = list(combinations(range(1, 4), 2))
+    for i in combination:
+        plan_matr = np.append(plan_matr, np.reshape(plan_matr[:, i[0]]*plan_matr[:, i[1]], (8, 1)), axis=1)
+        norm_matrix = np.append(norm_matrix, np.reshape(norm_matrix[:, i[0]]*norm_matrix[:, i[1]], (8, 1)), axis=1)
+    plan_matr = np.append(plan_matr, np.reshape(plan_matr[:, 1]*plan_matr[:, 2]*plan_matr[:, 3], (8, 1)), axis=1)
+    norm_matrix = np.append(norm_matrix, np.reshape(norm_matrix[:, 1]*norm_matrix[:, 2]*norm_matrix[:, 3], (8, 1)), axis=1)
 
-matrix_with_min_max_x = np.array([[10, 60], [-35, 15], [10, 15]])
-m = 6
-N = 8
-norm_matrix = np.array(list(product("01", repeat=3)), dtype=np.int)
-norm_matrix[norm_matrix == 0] = -1
-norm_matrix = np.insert(norm_matrix, 0, 1, axis=1)
-plan_matrix = np.empty((8, 3))
-for i in range(len(norm_matrix)):
-    for j in range(1, len(norm_matrix[i])):
-        if j == 1:
-            if norm_matrix[i, j] == -1:
-                plan_matrix[i, j-1] = 10
-            elif norm_matrix[i, j] == 1:
-                plan_matrix[i, j-1] = 60
-        elif j == 2:
-            if norm_matrix[i, j] == -1:
-                plan_matrix[i, j-1] = -35
-            elif norm_matrix[i, j] == 1:
-                plan_matrix[i, j-1] = 15
-        elif j == 3:
-            if norm_matrix[i, j] == -1:
-                plan_matrix[i, j-1] = 10
-            elif norm_matrix[i, j] == 1:
-                plan_matrix[i, j-1] = 15
-plan_matr = np.insert(plan_matrix, 0, 1, axis=1)
-Y_matrix = np.random.randint(200 + np.mean(matrix_with_min_max_x, axis=0)[0],
-                             200 + np.mean(matrix_with_min_max_x, axis=0)[1], size=(N, m))
-mean_Y = np.mean(Y_matrix, axis=1)
-combination = list(combinations(range(1, 4), 2))
-for i in combination:
-    plan_matr = np.append(plan_matr, np.reshape(plan_matr[:, i[0]]*plan_matr[:, i[1]], (8, 1)), axis=1)
-    norm_matrix = np.append(norm_matrix, np.reshape(norm_matrix[:, i[0]]*norm_matrix[:, i[1]], (8, 1)), axis=1)
-plan_matr = np.append(plan_matr, np.reshape(plan_matr[:, 1]*plan_matr[:, 2]*plan_matr[:, 3], (8, 1)), axis=1)
-norm_matrix = np.append(norm_matrix, np.reshape(norm_matrix[:, 1]*norm_matrix[:, 2]*norm_matrix[:, 3], (8, 1)), axis=1)
-
-if cochran_check(Y_matrix):
-    b_natura = np.linalg.lstsq(plan_matr, mean_Y, rcond=None)[0]
-    b_norm = np.linalg.lstsq(norm_matrix, mean_Y, rcond=None)[0]
-    check1 = np.sum(b_natura * plan_matr, axis=1)
-    check2 = np.sum(b_norm * norm_matrix, axis=1)
-    indexes = students_t_test(norm_matrix, Y_matrix)
-    print("Матриця плану експерименту: \n", plan_matr)
-    print("Нормована матриця: \n", norm_matrix)
-    print("Матриця відгуків: \n", Y_matrix)
-    print("Середні значення У: ", mean_Y)
-    print("Натуралізовані коефіціенти: ", b_natura)
-    print("Нормовані коефіціенти: ", b_norm)
-    print("Перевірка 1: ", check1)
-    print("Перевірка 2: ", check2)
-    print("Індекси коефіціентів, які задовольняють критерію Стьюдента: ", np.array(indexes)[0])
-    print("Критерій Стьюдента: ", np.sum(b_natura[indexes] * np.reshape(plan_matr[:, indexes], (N, np.size(indexes))), axis=1))
-    if phisher_criterion(Y_matrix, np.size(indexes)):
-        print("Рівняння регресії адекватно оригіналу.")
+    if cochran_check(Y_matrix):
+        b_natura = np.linalg.lstsq(plan_matr, mean_Y, rcond=None)[0]
+        b_norm = np.linalg.lstsq(norm_matrix, mean_Y, rcond=None)[0]
+        check1 = np.sum(b_natura * plan_matr, axis=1)
+        check2 = np.sum(b_norm * norm_matrix, axis=1)
+        indexes = students_t_test(norm_matrix, Y_matrix)
+        print("Матриця плану експерименту: \n", plan_matr)
+        print("Нормована матриця: \n", norm_matrix)
+        print("Матриця відгуків: \n", Y_matrix)
+        print("Середні значення У: ", mean_Y)
+        print("Натуралізовані коефіціенти: ", b_natura)
+        print("Нормовані коефіціенти: ", b_norm)
+        print("Перевірка 1: ", check1)
+        print("Перевірка 2: ", check2)
+        print("Індекси коефіціентів, які задовольняють критерію Стьюдента: ", np.array(indexes)[0])
+        print("Критерій Стьюдента: ", np.sum(b_natura[indexes] * np.reshape(plan_matr[:, indexes], (N, np.size(indexes))), axis=1))
+        if phisher_criterion(Y_matrix, np.size(indexes)):
+            print("Рівняння регресії адекватно оригіналу.")
+            break;
+        else:
+            print("Рівняння регресії неадекватно оригіналу.")
+            continue
     else:
-        print("Рівняння регресії неадекватно оригіналу.")
-else:
-    print("Дисперсія неоднорідна!")
+        print("Дисперсія неоднорідна!")
+        m += 1;
+        continue;
